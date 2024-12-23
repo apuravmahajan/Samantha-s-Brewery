968ac4ea77e603cb7d3b89b29a78769b8e871483
Apurav Mahajan <mahajanapurav991@gmail.com>
2024-12-23 23:28:31 +0530

    initial commit

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('', views.index, name = 'home'),
+    path('about', views.about, name = 'about'),
+    path('contact', views.contact, name = 'contact'),
+    path('products', views.products, name = 'products'),
+    path('books', views.books, name='books'),
+    path('menu', views.menu, name = 'menu')
+]
